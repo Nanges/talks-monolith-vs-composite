621b698ff59ab6f478ccb5be786d3a0e8cb588c4
olahaye <olahaye@actiris.be>
2023-03-25 14:25:02 +0100

    Fix errors

--- src/app/category-form/category-form.component.ts
+++ src/app/category-form/category-form.component.ts
@@ -6,7 +6,6 @@ import { CategoryService } from '../category.service';
 @Component({
     selector: 'app-category-form',
     templateUrl: './category-form.component.html',
-    styleUrls: ['./category-form.component.scss'],
 })
 export class CategoryFormComponent {
     readonly index: number;
