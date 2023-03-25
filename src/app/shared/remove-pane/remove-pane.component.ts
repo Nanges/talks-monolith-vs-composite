c1e4f7cae659fd301691e05c4ea01e8fc6412b3b
olahaye <olahaye@actiris.be>
2023-03-25 14:49:27 +0100

    Update remove-pane compoent

--- src/app/shared/remove-pane/remove-pane.component.ts
+++ src/app/shared/remove-pane/remove-pane.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-remove-pane',
-    template: `<button type="button" class="remove-btn">Remove</button>`,
+    template: `<button type="button" class="remove-btn" (click)="removeHandler()">Remove</button>`,
     host: {
         class: 'remove-pane',
     },
 })
-export class RemovePaneComponent {}
+export class RemovePaneComponent {
+    removeHandler() {}
+}
