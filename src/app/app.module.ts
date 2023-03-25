ce4e4242a9c1825f045c2ed94eddeebe34774fea
olahaye <olahaye@actiris.be>
2023-03-25 16:14:24 +0100

    Remove category-form.compoent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddCategoryFormComponent } from './add-category-form/add-category-form.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryFieldsComponent } from './category-fields/category-fields.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryScreenComponent } from './category-screen/category-screen.component';
 import { EditionLayoutComponent } from './shared/edition-layout/edition-layout.component';
 import { RemovePaneComponent } from './shared/remove-pane/remove-pane.component';
-import { AddCategoryFormComponent } from './add-category-form/add-category-form.component';
 import { UpdateCategoryFormComponent } from './update-category-form/update-category-form.component';
-import { CategoryFieldsComponent } from './category-fields/category-fields.component';
 
 @NgModule({
-    declarations: [AppComponent, CategoryListComponent, CategoryFormComponent, CategoryScreenComponent, EditionLayoutComponent, RemovePaneComponent, AddCategoryFormComponent, UpdateCategoryFormComponent, CategoryFieldsComponent],
+    declarations: [
+        AppComponent,
+        CategoryListComponent,
+        CategoryScreenComponent,
+        EditionLayoutComponent,
+        RemovePaneComponent,
+        AddCategoryFormComponent,
+        UpdateCategoryFormComponent,
+        CategoryFieldsComponent,
+    ],
     imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
     providers: [],
     bootstrap: [AppComponent],
