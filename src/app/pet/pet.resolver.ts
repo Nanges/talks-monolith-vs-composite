a48e39f82ecd718a3e622680842982cb99e5fe52
olahaye <olahaye@actiris.be>
2023-03-28 21:53:14 +0200

    Adapt resolver

--- src/app/pet/pet.resolver.ts
+++ src/app/pet/pet.resolver.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Pet, PetService } from './pet.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class PetResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
-  }
+export class PetResolver implements Resolve<Pet> {
+    /**
+     *
+     */
+    constructor(private petService: PetService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Pet {
+        const pet = this.petService.data[Number(route.paramMap.get('id'))];
+        if (pet !== undefined) {
+            return pet;
+        }
+
+        throw new Error('Pet not found');
+    }
 }
