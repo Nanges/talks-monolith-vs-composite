4bd722e976fd9525e09a7821704231e5812655b5
olahaye <olahaye@actiris.be>
2023-03-28 21:55:08 +0200

    Remove styles

--- src/app/pet/pet-routing.module.ts
+++ src/app/pet/pet-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PetFormComponent } from './pet-form/pet-form.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { PetScreenComponent } from './pet-screen/pet-screen.component';
+import { PetResolver } from './pet.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: PetScreenComponent,
+        children: [
+            {
+                path: 'create',
+                component: PetFormComponent,
+            },
+            {
+                path: ':id',
+                component: PetFormComponent,
+                resolve: {
+                    pet: PetResolver,
+                },
+            },
+            {
+                path: '**',
+                component: PetListComponent,
+            },
+        ],
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class PetRoutingModule { }
+export class PetRoutingModule {}
