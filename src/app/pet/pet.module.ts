424b4d440456b8686aed71d799f75dd1bf5d8ef2
olahaye <olahaye@actiris.be>
2023-03-28 22:54:07 +0200

    Add missing import

--- src/app/pet/pet.module.ts
+++ src/app/pet/pet.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PetFormComponent } from './pet-form/pet-form.component';
 import { PetListComponent } from './pet-list/pet-list.component';
@@ -8,6 +9,6 @@ import { PetScreenComponent } from './pet-screen/pet-screen.component';
 
 @NgModule({
     declarations: [PetFormComponent, PetListComponent, PetScreenComponent],
-    imports: [CommonModule, PetRoutingModule],
+    imports: [CommonModule, PetRoutingModule, ReactiveFormsModule],
 })
 export class PetModule {}
