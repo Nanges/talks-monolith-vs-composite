2c6ecddbacd8d805525eec01ae7fad81c2dee99b
olahaye <olahaye@actiris.be>
2023-04-04 17:17:58 +0200

    Adapt addCategoryForm

--- src/app/category/add-category-form/add-category-form.component.ts
+++ src/app/category/add-category-form/add-category-form.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BaseCategoryForm } from '../base-category-form';
 import { CategoryService } from '../category.service';
 
 @Component({
     selector: 'app-add-category-form',
     template: `
-        <app-edition-layout io [title]="'Add category'" [form]="form" (save)="saveHandler()">
+        <app-edition-layout io [title]="'Add category'" [form]="form" (save)="save()">
             <app-category-fields></app-category-fields>
         </app-edition-layout>
     `,
 })
-export class AddCategoryFormComponent extends BaseCategoryForm {
+export class AddCategoryFormComponent {
+    readonly form: FormGroup<any>;
+
     constructor(private categoryService: CategoryService, private router: Router, private route: ActivatedRoute) {
-        super();
+        this.form = new FormGroup({
+            category: new FormControl(),
+        });
     }
 
-    saveHandler(): void {
+    save(): void {
         this.categoryService.create(this.form.value['category']);
         this.router.navigate(['../'], { relativeTo: this.route });
     }
